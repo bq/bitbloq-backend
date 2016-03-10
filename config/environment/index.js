c2517984ee2060a14e42e86624e0c370dfad91b5
Dieguin <diego.segura@bq.com>
2016-03-10 18:13:21 +0100

    remove passport add auth improvements

--- config/environment/index.js
+++ config/environment/index.js
@@ -8,58 +8,40 @@ var _ = require('lodash');
 // All configurations will extend these options
 // ============================================
 var all = {
-  env: process.env.NODE_ENV,
+    env: process.env.NODE_ENV,
 
-  // Root path of server
-  root: path.normalize(__dirname + '/../../..'),
+    // Root path of server
+    root: path.normalize(__dirname + '/../../..'),
 
-  // Server port
-  port: process.env.PORT || 8000,
+    // Server port
+    port: process.env.PORT || 8000,
 
-  // Server IP
-  ip: process.env.IP || '0.0.0.0',
+    // Server IP
+    ip: process.env.IP || '0.0.0.0',
 
-  // Should we populate the DB with sample data?
-  seedDB: false,
+    // Should we populate the DB with sample data?
+    seedDB: false,
 
-  // Secret for session, you will want to change this and make it an environment variable
-  secrets: {
-    session: 'bitbloq-secret'
-  },
+    // Secret for session, you will want to change this and make it an environment variable
+    secrets: {
+        session: 'bitbloq-secret'
+    },
 
-  // List of user roles
-  userRoles: ['guest', 'user', 'admin'],
+    // List of user roles
+    userRoles: ['guest', 'user', 'admin'],
 
-  // MongoDB connection options
-  mongo: {
-    options: {
-      db: {
-        safe: true
-      }
+    // MongoDB connection options
+    mongo: {
+        options: {
+            db: {
+                safe: true
+            }
+        }
     }
-  },
-
-  facebook: {
-    clientID: process.env.FACEBOOK_ID || '618672994918034',
-    clientSecret: process.env.FACEBOOK_SECRET || 'secret',
-    callbackURL: (process.env.DOMAIN || '') + '/auth/facebook/callback'
-  },
-
-  twitter: {
-    clientID: process.env.TWITTER_ID || 'id',
-    clientSecret: process.env.TWITTER_SECRET || 'secret',
-    callbackURL: (process.env.DOMAIN || '') + '/auth/twitter/callback'
-  },
-
-  google: {
-    clientID: process.env.GOOGLE_ID || '846494928917-uj5odicj3fe7jn0in4a6lko0v0diuic2.apps.googleusercontent.com',
-    clientSecret: process.env.GOOGLE_SECRET || 'AIzaSyA4NIAP4k3TA0kpo6POxWcS_2-Rpj_JaoE',
-    callbackURL: (process.env.DOMAIN || '') + '/auth/google/callback'
-  }
 };
 
 // Export the config object based on the NODE_ENV
 // ==============================================
 module.exports = _.merge(
-  all,
-  require('./' + process.env.NODE_ENV + '.js') || {});
+    all,
+    require('./' + process.env.NODE_ENV + '.js') || {});
