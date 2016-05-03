a1e6a08f30229f27f132f2cd9949738beb817051
LauraDRA <laura.delrio.aviles@gmail.com>
2016-05-03 15:23:19 +0200

    feedback with bluebird

--- api/feedback/feedback.controller.js
+++ api/feedback/feedback.controller.js
@@ -10,15 +10,19 @@ var Feedback = require('./feedback.model'),
 exports.create = function(req, res) {
     console.log('CREANDO QUEJA');
     var newProject = new Feedback(req.body);
-    newProject.save().then(function(feedback) {
-        var email = 'laura.delrio@bq.com'; //todo poner aqui a soporte
-        var locals = {
+    newProject.save(function(err, feedback) {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            var email = 'laura.delrio@bq.com'; //todo poner aqui a soporte
+            var locals = {
                 email: email,
                 subject: 'Nuevo feedback'
             };
 
-        //todo cambiar plantilla
-        mailer.sendOne('password_reset', locals);
-        res.sendStatus(200);
-    }).catch(utils.validationError(res));
+            //todo cambiar plantilla
+            mailer.sendOne('password_reset', locals);
+            res.sendStatus(200);
+        }
+    });
 };
--- api/feedback/feedback.model.js
+++ api/feedback/feedback.model.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var mongoose = require('bluebird').promisifyAll(require('mongoose'));
+var mongoose = require('mongoose');
 
 var FeedbackSchema = new mongoose.Schema({
     message : String,
