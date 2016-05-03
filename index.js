a55d3fbf81bfd1a095151be27ea6cc4c856f388d
Tom Calvo <calvo.tom@gmail.com>
2016-05-03 15:16:58 +0200

    eliminados ficheros que no se usan.

--- index.js
+++ index.js
@@ -3,7 +3,5 @@
 // Set default node environment to development
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-if (env === 'development' || env === 'test') {}
-
 // Export the application
 exports = module.exports = require('./app');
