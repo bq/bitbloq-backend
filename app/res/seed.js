1604ca8768cce0495090ef97a22684e97bded052
LauraDRA <laura.delrio.aviles@gmail.com>
2016-06-07 10:54:50 +0200

    seed

--- app/res/seed.js
+++ app/res/seed.js
@@ -20,53 +20,53 @@ Version.find({}).remove(function() {
     });
 });
 
-//
-// User.find({}).remove(function() {
-//     return User.create({
-//         provider: 'local',
-//         name: 'Test User',
-//         email: 'test@example.com',
-//         password: 'test'
-//     }, {
-//         provider: 'local',
-//         role: 'admin',
-//         name: 'Admin',
-//         email: 'admin@example.com',
-//         password: 'admin'
-//     }, function() {
-//         console.log('finished populating users');
-//     });
-// });
-//
-// Token.find({}).remove(function() {
-//     return Token.create({
-//         "userId": "123456",
-//         "token": "987654"
-//     }, function() {
-//         console.log('finished populating tokens');
-//     });
-// });
-//
-// Project.find({}).remove(function() {
-//     return Project.create({
-//         "name": "proyecto1",
-//         "description": "mi descripcion",
-//         "code": "mi code",
-//         "_acl": {
-//             'user:57164392527b27df52dbe734': {
-//                 permission: 'ADMIN'
-//             }
-//         }
-//     }, {
-//         "name": "proyecto2",
-//         "description": "otra mas",
-//         "code": "codecodecodecode",
-//         "_acl": {
-//             'user:57164392527b27df52dbe734': {
-//                 permission: 'READ'
-//             }
-//         }
-//     }, function() {
-//         console.log('finished populating projects');
-//     });
-// });
+
+User.find({}).remove(function() {
+    return User.create({
+        provider: 'local',
+        name: 'Test User',
+        email: 'test@example.com',
+        password: 'test'
+    }, {
+        provider: 'local',
+        role: 'admin',
+        name: 'Admin',
+        email: 'admin@example.com',
+        password: 'admin'
+    }, function() {
+        console.log('finished populating users');
+    });
+});
+
+Token.find({}).remove(function() {
+    return Token.create({
+        "userId": "123456",
+        "token": "987654"
+    }, function() {
+        console.log('finished populating tokens');
+    });
+});
+
+Project.find({}).remove(function() {
+    return Project.create({
+        "name": "proyecto1",
+        "description": "mi descripcion",
+        "code": "mi code",
+        "_acl": {
+            'user:57164392527b27df52dbe734': {
+                permission: 'ADMIN'
+            }
+        }
+    }, {
+        "name": "proyecto2",
+        "description": "otra mas",
+        "code": "codecodecodecode",
+        "_acl": {
+            'user:57164392527b27df52dbe734': {
+                permission: 'READ'
+            }
+        }
+    }, function() {
+        console.log('finished populating projects');
+    });
+});
