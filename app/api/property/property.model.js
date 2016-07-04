9801d3cab0dac9ca10c2fb8854190cc9f56c79e7
Tom Calvo <calvo.tom@gmail.com>
2016-07-04 11:21:16 +0200

    added robotsFirmwareVersion to model

--- app/api/property/property.model.js
+++ app/api/property/property.model.js
@@ -5,6 +5,7 @@ var mongoose = require('mongoose');
 var PropertySchema = new mongoose.Schema({
     web2boardVersion : String,
     bitbloqLibsVersion : String,
+    robotsFirmwareVersion:{},
     bloqsSortTree : {}
 }, {
     timestamps: true
