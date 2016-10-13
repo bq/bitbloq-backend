058f23f05430ba6c0653fae7fe90ffcd92d31a79
LauraDRA <laura.delrio.aviles@gmail.com>
2016-10-13 04:03:14 +0200

    mode center estructure

---
+++ app/api/centerMode/center.controller.js
@@ -0,0 +1,136 @@
+'use strict';
+
+/**
+ * Create an exercise
+ */
+exports.createExercise = function(req, res) {
+
+};
+
+/**
+ * Create task
+ */
+exports.createTask = function(req, res) {
+
+};
+
+/**
+ * Create group
+ */
+exports.createGroup = function(req, res) {
+
+};
+
+/**
+ * Create center
+ */
+exports.createCenter = function(req, res) {
+
+};
+
+/**
+ * Get info of exercise by id
+ */
+exports.getExercise = function(req, res) {
+
+};
+
+/**
+ * Get a exercise by its task
+ */
+exports.getExerciseByTask = function(req, res) {
+
+};
+
+/**
+ * Get student task
+ */
+exports.getTask = function(req, res) {
+
+};
+
+/**
+ * Get student group
+ */
+exports.getGroup = function(req, res) {
+
+};
+
+/**
+ * Get student group by its teacher
+ */
+exports.getGroupByTeacher = function(req, res) {
+
+};
+
+/**
+ * Get center
+ */
+exports.getCenter = function(req, res) {
+
+};
+
+/**
+ * Update an exercise ir user is owner
+ */
+exports.updateExercise = function(req, res) {
+
+};
+
+/**
+ * Update a task if user is owner
+ */
+exports.updateTask = function(req, res) {
+
+};
+
+/**
+ * Update a student group
+ */
+exports.updateGroup = function(req, res) {
+
+};
+
+/**
+ * Update center information
+ */
+exports.updateCenter = function(req, res) {
+
+};
+
+/**
+ * Delete an exercise if user is owner
+ */
+exports.deleteExercise = function(req, res) {
+
+};
+
+/**
+ * Delete a group if user is owner
+ */
+exports.deleteGroup = function(req, res) {
+
+};
+
+
+/**
+ * Delete a task if user is owner
+ */
+exports.deleteTask = function(req, res) {
+
+};
+
+
+/**
+ * Delete a group if user is owner
+ */
+exports.destroyGroup = function(req, res) {
+
+};
+
+/**
+ * Make anonymous a center if user is owner
+ */
+exports.anonCenter = function(req, res) {
+
+};
---
+++ app/api/centerMode/index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var express = require('express'),
+    controller = require('./center.controller.js'),
+    auth = require('../../components/auth/auth.service');
+
+var router = express.Router();
+
+// GET
+router.get('/exercise/:id', auth.isAuthenticated(), controller.getExercise);
+router.get('/exercise/:task', auth.isAuthenticated(), controller.getExerciseByTask);
+router.get('/task/:id', auth.isAuthenticated(), controller.getTask);
+router.get('/group/:id', auth.isAuthenticated(), controller.getGroup);
+router.get('/group/teacher/:id', auth.isAuthenticated(), controller.getGroupByTeacher);
+router.get('/center/:id', auth.isAuthenticated(), controller.getCenter);
+
+// POST
+router.post('/exercise', auth.isAuthenticated(), controller.createExercise);
+router.post('/task', auth.isAuthenticated(), controller.createTask);
+router.post('/group', auth.isAuthenticated(), controller.createGroup);
+router.post('/center', auth.isAuthenticated(), controller.createCenter);
+
+// PUT
+router.put('/exercise/:id', auth.isAuthenticated(), controller.updateExercise);
+router.put('/task/:id', auth.isAuthenticated(), controller.updateTask);
+router.put('/group/:id', auth.isAuthenticated(), controller.updateGroup);
+router.put('/center/:id', auth.isAuthenticated(), controller.updateCenter);
+
+// DELETE
+router.delete('/exercise/:id', auth.isAuthenticated(), controller.deleteExercise);
+router.delete('/task/:id', auth.isAuthenticated(), controller.deleteTask);
+router.delete('/group/:id', auth.isAuthenticated(), controller.deleteGroup);
+router.delete('/center/:id', auth.isAuthenticated(), controller.anonCenter); //anonymous
+
+module.exports = router;
