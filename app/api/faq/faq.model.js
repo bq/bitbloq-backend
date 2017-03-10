d027d9521c0677636a7608b6caf79fb4220d1d7f
LauraDRA <laura.delrio.aviles@gmail.com>
2017-03-10 11:20:37 +0100

    [SWBIT-2065] logic deleted on faq

--- app/api/faq/faq.model.js
+++ app/api/faq/faq.model.js
@@ -8,7 +8,41 @@ var FaqSchema = new mongoose.Schema({
     order: {
         type: Number,
         default: 0
-    }
+    },
+    deleted: Boolean
 });
+
+/**
+ * Pre hook
+ */
+
+function findNotDeletedMiddleware(next) {
+    this.where('deleted').in([false, undefined, null]);
+    next();
+}
+
+FaqSchema.pre('find', findNotDeletedMiddleware);
+FaqSchema.pre('findOne', findNotDeletedMiddleware);
+FaqSchema.pre('findOneAndUpdate', findNotDeletedMiddleware);
+FaqSchema.pre('count', findNotDeletedMiddleware);
+
+
+/**
+ * Methods
+ */
+
+FaqSchema.methods = {
+
+    /**
+     * delete - change deleted attribute to true
+     *
+     * @param {Function} next
+     * @api public
+     */
+    delete: function(next) {
+        this.deleted = true;
+        this.save(next);
+    }
+};
 
 module.exports = mongoose.model('Faq', FaqSchema);
