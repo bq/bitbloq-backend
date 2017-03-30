5dcd463e1f529ffef38bc59995cc2061312fc37b
LauraDRA <laura.delrio.aviles@gmail.com>
2017-03-30 17:36:06 +0200

    [SWBIT-2148] kit api

---
+++ app/api/hardware/kit/kit.model.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var mongoose = require('mongoose');
+
+var KitSchema = new mongoose.Schema({
+    uuid: String,
+    name: String,
+    manufacturer: String,
+    board: String,
+    components: Array,
+    underDevelopment: Boolean,
+    deleted: Boolean
+}, {
+    timestamps: true
+});
+
+/**
+ * Pre hook
+ */
+
+function findNotDeletedMiddleware(next) {
+    this.where('deleted').in([false, undefined, null]);
+    next();
+}
+
+KitSchema.pre('find', findNotDeletedMiddleware);
+KitSchema.pre('findOne', findNotDeletedMiddleware);
+KitSchema.pre('findOneAndUpdate', findNotDeletedMiddleware);
+KitSchema.pre('count', findNotDeletedMiddleware);
+
+
+/**
+ * Methods
+ */
+
+KitSchema.methods = {
+
+    /**
+     * delete - change deleted attribute to true
+     *
+     * @param {Function} next
+     * @api public
+     */
+    delete: function(next) {
+        this.deleted = true;
+        this.save(next);
+    }
+};
+
+module.exports = mongoose.model('hardware-kit', KitSchema);
